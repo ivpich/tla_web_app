01534c52fa60d4bc31073a7959873d2ea2bf610d
ivpich <iv.pichugin@mail.ru>
2023-05-28 17:29:05 +0300

    app v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import UserProfile from './UserProfile';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const onClose = () => {
+        tg.close();
+    }
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+
+    const user = {
+        avatar: '/circle_logo.png',
+        firstName: 'Глеб',
+        lastName: 'Суханов',
+        occupation: 'TLA chief interlocker',
+        role: 'Основатель',
+        trustPoints: 75,
+    }
+
+    return (
+        <div className="App">
+            <UserProfile user={user}/>
+            <button className="closeButton" onClick={onClose}>Закрыть профиль</button>
+        </div>
+    );
 }
 
 export default App;
